c0fcf44fd843202dc8bfd4cb383ad3ff27189c74
daniele <zeolla.d@gmail.com>
2024-12-02 20:34:21 +0100

    Part2 01

--- 01/main.go
+++ 01/main.go
@@ -24,6 +24,62 @@ func find_min_and_reset(numbers *[]int) int {
 	return smallest
 }
 
+func part_1(file *os.File) int {
+	var left_numbers []int
+	var right_numbers []int
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		data := strings.Split(line, "   ")
+
+		num, _ := strconv.Atoi(data[0])
+		left_numbers = append(left_numbers, num)
+
+		num, _ = strconv.Atoi(data[1])
+		right_numbers = append(right_numbers, num)
+
+	}
+
+	distance := 0
+	for i := 0; i < len(left_numbers); i++ {
+		rel_diff := find_min_and_reset(&left_numbers) - find_min_and_reset(&right_numbers)
+		distance += int(math.Abs(float64(rel_diff)))
+	}
+
+	return distance
+}
+
+func part_2(file *os.File) int {
+	scanner := bufio.NewScanner(file)
+	var num_map = make(map[int]int)
+	var right_nums []int
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		data := strings.Split(line, "   ")
+
+		left_num, _ := strconv.Atoi(data[0])
+		right_num, _ := strconv.Atoi(data[1])
+
+		num_map[left_num] = 0
+		right_nums = append(right_nums, right_num)
+	}
+
+	for _, num := range right_nums {
+		if _, ok := num_map[num]; ok {
+			num_map[num] = num_map[num] + 1
+		}
+	}
+
+	var score = 0
+	for key, val := range num_map {
+		score = score + (key * val)
+	}
+
+	return score
+}
+
 func main() {
 
 	if len(os.Args) < 2 || os.Args[1] == "" {
@@ -40,33 +96,11 @@ func main() {
 
 	defer file.Close()
 
-	var left_numbers []int
-	var right_numbers []int
+	distance := part_1(file)
+	file.Seek(0, 0)
+	score := part_2(file)
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
-		data := strings.Split(line, "   ")
-
-		if len(data) != 2 {
-			fmt.Println("Error: line does not contain two numbers")
-			continue
-		}
-
-		num, _ := strconv.Atoi(data[0])
-		left_numbers = append(left_numbers, num)
-
-		num, _ = strconv.Atoi(data[1])
-		right_numbers = append(right_numbers, num)
-
-	}
-
-	distance := 0
-	for i := 0; i < len(left_numbers); i++ {
-		rel_diff := find_min_and_reset(&left_numbers) - find_min_and_reset(&right_numbers)
-		distance += int(math.Abs(float64(rel_diff)))
-	}
-
-	fmt.Println("The total distance is ", distance)
+	fmt.Println("part1 sol:", distance)
+	fmt.Println("part2 sol:", score)
 
 }
